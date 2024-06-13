cdf4bb46bd3a636c140e218d7fb4f12c8e4d02ca
Simon Prickett <simon@crudworks.org>
2024-06-13 19:08:10 +0100

    Added script to authenticate with Twitter.

---
+++ twitter_auth.py
@@ -0,0 +1,20 @@
+from twython import Twython
+import argparse
+
+parser = argparse.ArgumentParser("twitter_auth")
+parser.add_argument("--key", help="Your app API key from Twitter.", required=True)
+parser.add_argument("--secret", help="Your app API secret from Twitter.", required=True)
+args = parser.parse_args()
+
+twitter = Twython(args.key, args.secret)
+auth = twitter.get_authentication_tokens()
+print(auth)
+
+f = open("settings.py", "w")
+f.write(f"app_key={args.key}\n")
+f.write(f"app_secret={args.secret}\n")
+f.write(f"oauth_token={auth['oauth_token']}\n")
+f.write(f"oauth_token_secret={auth['oauth_token_secret']}\n")
+f.close()
+
+print("Created settings.py")
