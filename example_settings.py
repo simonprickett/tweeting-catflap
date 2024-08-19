15ffa8b81aad9862ae5ef4f1ac9910c274ef5886
Simon Prickett <simon@crudworks.org>
2024-08-19 16:54:16 +0100

    Added example settings file.

---
+++ example_settings.py
@@ -0,0 +1,13 @@
+# Example settings.py.  Rename this file to settings.py and
+# replace the placeholders with your own values.  Do not
+# commit this file to source control or share the contents!
+app_key="<TWITTER APP KEY>"
+app_secret="<TWITTER APP SECRET>"
+oauth_token="<TWITTER OAUTH TOKEN>"
+oauth_token_secret="<TWITTER OAUTH TOKEN SECRET>"
+bluesky_user="<BLUESKY USER/DOMAIN>"
+bluesky_pass="<BLUESKY PASSWORD>"
+# These settings control which social networks to post to.
+# Use True or False - case sensitive, no quotation marks.
+post_bluesky=True
+post_twitter=False
